de1514658211ae5eecf34383aa78e405b964f801
depso123 <l.parvanov2017@pmg-pernik.com>
2022-02-25 12:58:31 +0200

    New

---
+++ Eventure/Eventures/Controllers/OrdersController.cs
@@ -0,0 +1,48 @@
+﻿using Eventures.App.Models;
+using Eventures.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Eventures.App.Controllers
+{
+    [Authorize]
+    public class OrdersController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public OrdersController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpPost]
+
+        public IActionResult Create(OrderCreateBindingModel bindingModel)
+        {
+            if (this.ModelState.IsValid)
+            {
+                string currentUserId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var user = this.context.Users.SingleOrDefault(u => u.Id == currentUserId);
+                var ev = this.context.Users.SingleOrDefault(u => u.Id == currentUserId);
+                this.context.Events.SingleOrDefault(e => e.Id == bindingModel.EventId);
+                if (user == null || ev == null || ev.TotalTickets < bindingModel.TicketsCount)
+                {
+
+                    return this.RedirectToAction("All", "Events");
+                }
+
+
+
+
+            }
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Eventure/Eventures/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Eventures.App.Domain;
+using Eventures.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventures.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<EventuresUser>
+    {
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        
+    }
+}
